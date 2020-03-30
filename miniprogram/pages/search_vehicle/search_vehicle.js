d88fc86711a4ce4bb75ff88e9d30108c758697e9
npukujui11 <npukujui11@gmail.com>
2020-03-30 17:10:05 +0800

    部分完善了查找界面的wxml和js
    界面有待修复

--- miniprogram/pages/search_vehicle/search_vehicle.js
+++ miniprogram/pages/search_vehicle/search_vehicle.js
@@ -5,62 +5,77 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    index_vehicle_engine: 0,
+    index_vehicle_transmission: 0,
+    index_vehicle_phase: 0,
+    index_vehicle_ecusupplier: 0,
+    index_vehicle_emission: 0,
+    index_vehicle_configuration: 0,
+    vehicle_engine: ['245T', '280T', '320T', 'B15', 'N15A', 'N15T'],
+    vehicle_transmission: ['CTF25A', 'CTF25B', 'CTF25D', 'CTF28E', 'CTF35', 'Hybrid Power'],
+    vehicle_phase: ['MULE', 'NS', 'OTS', 'PRO', 'S'],
+    vehicle_ecusupplier: ['Conti', 'Delphi', 'UAES'],
+    vehicle_emission: ['CN5', 'CN6', 'BS6'],
+    vehicle_configuration: ['LV0', 'LV1', 'LV2', 'LV3', 'LV4'],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  bindvehicle_engineChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index_vehicle_engine: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  bindvehicle_transmissionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index_vehicle_transmission: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  bindvehicle_phaseChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index_vehicle_phase: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  bindvehicle_ecusupplierChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index_vehicle_ecusupplier: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  bindvehicle_emissionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index_vehicle_emission: e.detail.value
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  bindvehicle_configurationChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index_vehicle_configuration: e.detail.value
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  bindMultiPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndex: e.detail.value
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
   }
 })
