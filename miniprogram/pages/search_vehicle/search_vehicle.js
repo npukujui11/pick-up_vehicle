7097b4d0f2bde903436c93d3eb924151f12ea641
nyannnnnpass <fst1997@126.com>
2020-03-31 20:06:26 +0800

    2020.3.31 20:06

--- miniprogram/pages/search_vehicle/search_vehicle.js
+++ miniprogram/pages/search_vehicle/search_vehicle.js
@@ -1,105 +1,44 @@
 // miniprogram/pages/search_vehicle/search_vehicle.js
+const db = wx.cloud.database()
+const _ = db.command
+const $ = db.command.aggregate
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-    index_vehicle_engine: 0,
-    index_vehicle_transmission: 0,
-    index_vehicle_phase: 0,
-    index_vehicle_ecusupplier: 0,
-    index_vehicle_emission: 0,
-    index_vehicle_configuration: 0,
-    default_vehicle_engine: '请选择发动机',
-    default_vehicle_transmission: '请选择变速器',
-    default_vehicle_phase: '请选择车辆阶段',
-    default_vehicle_ecusupplier: '请选择ECU供应商',
-    default_vehicle_emission: '请选择排放',
-    default_vehicle_configuration: '请选择配置',
-    vehicle_engine: ['245T', '280T', '320T', 'B15', 'N15A', 'N15T'],
-    vehicle_transmission: ['CTF25A', 'CTF25B', 'CTF25D', 'CTF28E', 'CTF35', 'Hybrid Power'],
-    vehicle_phase: ['MULE', 'NS', 'OTS', 'PRO', 'S'],
-    vehicle_ecusupplier: ['Conti', 'Delphi', 'UAES'],
-    vehicle_emission: ['CN5', 'CN6', 'BS6'],
-    vehicle_configuration: ['LV0', 'LV1', 'LV2', 'LV3', 'LV4'],
+
+    indexEngine: 0,
+    dataEngine: ["任意"]
+
   },
-  bindvehicle_engineChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    let index_vehicle_engine = e.detail.value
-    let vehicle_engine = this.data.vehicle_engine
-    this.setData({
-      index_vehicle_engine,
-      default_vehicle_engine: vehicle_engine[index_vehicle_engine]
+
+  onLoad: function (event) {
+    //获取发动机种类
+    db.collection('storage').aggregate().group({
+      _id: null,
+      engineType: $.addToSet('$发动机') 
+    }).end().then(res => {
+      //console.log(res.list[0].engineType)
+      var add = res.list[0].engineType
+      var target = this.data.dataEngine
+      target.push(...add)
+      //console.log(target)
+      this.setData({
+        dataEngine: target
+      })
     })
   },
-  bindvehicle_transmissionChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    let index_vehicle_transmission = e.detail.value
-    let vehicle_transmission = this.data.vehicle_transmission
+
+  bindChangeEngine: function (event) {
+    //更新picker的index
     this.setData({
-      index_vehicle_transmission,
-      default_vehicle_transmission: vehicle_transmission[index_vehicle_transmission]
+      indexEngine: event.detail.value
     })
   },
-  bindvehicle_phaseChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    let index_vehicle_phase = e.detail.value
-    let vehicle_phase = this.data.vehicle_phase
-    this.setData({
-      index_vehicle_phase,
-      default_vehicle_phase: vehicle_phase[index_vehicle_phase]
-    })
-  },
-  bindvehicle_ecusupplierChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    let index_vehicle_ecusupplier = e.detail.value
-    let vehicle_ecusupplier = this.data.vehicle_ecusupplier
-    this.setData({
-      index_vehicle_ecusupplier,
-      default_vehicle_ecusupplier: vehicle_ecusupplier[index_vehicle_ecusupplier]
-    })
-  },
-  bindvehicle_emissionChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    let index_vehicle_emission = e.detail.value
-    let vehicle_emission = this.data.vehicle_emission
-    this.setData({
-      index_vehicle_emission,
-      default_vehicle_emission: vehicle_emission[index_vehicle_emission]
-    })
-  },
-  bindvehicle_configurationChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    let index_vehicle_configuration = e.detail.value
-    let vehicle_configuration = this.data.vehicle_configuration
-    this.setData({
-      index_vehicle_configuration,
-      default_vehicle_configuration: vehicle_configuration[index_vehicle_configuration]
-    })
-  },
-  bindMultiPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      multiIndex: e.detail.value
-    })
-  },
-  bindDateChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      date: e.detail.value
-    })
-  },
-  bindTimeChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      time: e.detail.value
-    })
-  },
-  bindRegionChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      region: e.detail.value
-    })
+
+  onSearchButton: function (event) {
+    //构造查询请求语句
+    //页面跳转
   }
+
 })
