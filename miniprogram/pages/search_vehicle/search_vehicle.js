0d23f2017c4db503c99de9bee7dbdb41f285f32f
npukujui11 <npukujui11@gmail.com>
2020-03-31 14:45:27 +0800

    设置了默认请选择
    例如发动机：请选择发动机

--- miniprogram/pages/search_vehicle/search_vehicle.js
+++ miniprogram/pages/search_vehicle/search_vehicle.js
@@ -11,6 +11,12 @@ Page({
     index_vehicle_ecusupplier: 0,
     index_vehicle_emission: 0,
     index_vehicle_configuration: 0,
+    default_vehicle_engine: '请选择发动机',
+    default_vehicle_transmission: '请选择变速器',
+    default_vehicle_phase: '请选择车辆阶段',
+    default_vehicle_ecusupplier: '请选择ECU供应商',
+    default_vehicle_emission: '请选择排放',
+    default_vehicle_configuration: '请选择配置',
     vehicle_engine: ['245T', '280T', '320T', 'B15', 'N15A', 'N15T'],
     vehicle_transmission: ['CTF25A', 'CTF25B', 'CTF25D', 'CTF28E', 'CTF35', 'Hybrid Power'],
     vehicle_phase: ['MULE', 'NS', 'OTS', 'PRO', 'S'],
@@ -20,38 +26,56 @@ Page({
   },
   bindvehicle_engineChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
+    let index_vehicle_engine = e.detail.value
+    let vehicle_engine = this.data.vehicle_engine
     this.setData({
-      index_vehicle_engine: e.detail.value
+      index_vehicle_engine,
+      default_vehicle_engine: vehicle_engine[index_vehicle_engine]
     })
   },
   bindvehicle_transmissionChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
+    let index_vehicle_transmission = e.detail.value
+    let vehicle_transmission = this.data.vehicle_transmission
     this.setData({
-      index_vehicle_transmission: e.detail.value
+      index_vehicle_transmission,
+      default_vehicle_transmission: vehicle_transmission[index_vehicle_transmission]
     })
   },
   bindvehicle_phaseChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
+    let index_vehicle_phase = e.detail.value
+    let vehicle_phase = this.data.vehicle_phase
     this.setData({
-      index_vehicle_phase: e.detail.value
+      index_vehicle_phase,
+      default_vehicle_phase: vehicle_phase[index_vehicle_phase]
     })
   },
   bindvehicle_ecusupplierChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
+    let index_vehicle_ecusupplier = e.detail.value
+    let vehicle_ecusupplier = this.data.vehicle_ecusupplier
     this.setData({
-      index_vehicle_ecusupplier: e.detail.value
+      index_vehicle_ecusupplier,
+      default_vehicle_ecusupplier: vehicle_ecusupplier[index_vehicle_ecusupplier]
     })
   },
   bindvehicle_emissionChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
+    let index_vehicle_emission = e.detail.value
+    let vehicle_emission = this.data.vehicle_emission
     this.setData({
-      index_vehicle_emission: e.detail.value
+      index_vehicle_emission,
+      default_vehicle_emission: vehicle_emission[index_vehicle_emission]
     })
   },
   bindvehicle_configurationChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
+    let index_vehicle_configuration = e.detail.value
+    let vehicle_configuration = this.data.vehicle_configuration
     this.setData({
-      index_vehicle_configuration: e.detail.value
+      index_vehicle_configuration,
+      default_vehicle_configuration: vehicle_configuration[index_vehicle_configuration]
     })
   },
   bindMultiPickerChange: function (e) {
